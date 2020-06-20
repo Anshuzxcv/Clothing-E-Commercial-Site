844d1d32e7908a4962ce7d9092e8b1f859faf6fe
Anshuzxcv <anshuzxcv@gmail.com>
2020-06-21 01:54:46 +0530

    implemented firebase utilis anddinclude ability to store authenticated user into our firebase database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import InputForm from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { SignInWithGoogle } from '../../firebase/firebase.jtilis';
+import { auth, SignInWithGoogle } from '../../firebase/firebase.jtilis';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -15,10 +15,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+    const { email, password } = this.state;
 
-    this.setState({ name: '', password: '' });
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
@@ -33,7 +39,7 @@ class SignIn extends React.Component {
         <h2>Already have an account</h2>
         <span>Sign in with Your Email and Password</span>
 
-        <form onClick={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <InputForm
             type='email'
             name='email'
@@ -54,8 +60,8 @@ class SignIn extends React.Component {
 
           <div className='buttons'>
             <CustomButton type='submit'>Sign In</CustomButton>
-            <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>
-              Sign in with Google{' '}
+            <CustomButton type="button" onClick={SignInWithGoogle} isGoogleSignIn>
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
